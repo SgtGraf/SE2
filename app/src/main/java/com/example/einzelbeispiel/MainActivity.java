6dc2301eac1b652fdf2f3f3fac7e2c47a4907edc
Roman <rograf@edu.aau.at>
2020-03-15 14:14:31 +0100

    initialised buttons and textfield in main activity. updated Text

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.einzelbeispiel;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setText("Los!");
+        EditText edit = (EditText)findViewById(R.id.editText);
+        TextView textTop = (TextView)findViewById(R.id.textView);
+        textTop.setText("Matrikelnummer");
+        TextView textBottom = (TextView)findViewById(R.id.textView2);
+        textBottom.setText("...");
     }
 }
